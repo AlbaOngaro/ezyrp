1979320e6d83b883889f7f70896a1d4fdccfb61e
Alba Ongaro <alba.ongaro@reprisk.com>
2023-08-20 15:15:45 +0200

    feat: added new textarea and select components

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -1,5 +1,5 @@
 import * as Form from "@radix-ui/react-form";
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
 import { twMerge } from "lib/utils/twMerge";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
@@ -9,50 +9,50 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
 }
 
-export function Input({
-  label,
-  description,
-  validations,
-  name,
-  className,
-  ...rest
-}: Props) {
+export const Input = forwardRef<HTMLInputElement, Props>(function Input(
+  { label, description, validations, name, className, ...rest },
+  ref,
+) {
   return (
     <Form.Field
+      ref={ref}
       className={twMerge("flex flex-col gap-2", className)}
       name={name}
       id={name}
+      asChild
     >
-      {(label || description) && (
-        <Form.Label className="flex flex-col text-sm font-bold text-gray-800">
-          {label}
-          {description && (
-            <small className="text-sm font-normal text-dark-blue-gray">
-              {description}
-            </small>
-          )}
-        </Form.Label>
-      )}
+      <fieldset>
+        {(label || description) && (
+          <Form.Label className="flex flex-col text-sm font-bold text-gray-800">
+            {label}
+            {description && (
+              <small className="text-sm font-normal text-dark-blue-gray">
+                {description}
+              </small>
+            )}
+          </Form.Label>
+        )}
 
-      <Form.Control asChild>
-        <input
-          className="m-0 resize-none py-2 px-4 text-sm bg-white rounded outline-none transition-all duration-300 border border-solid border-gray-300 focus:ring-0 focus:outline-none focus:border-gray-500 hover:border-gray-500 data-[invalid=true]:text-red-400 data-[invalid=true]:border-red-400"
-          name={name}
-          id={name}
-          {...rest}
-        />
-      </Form.Control>
+        <Form.Control asChild>
+          <input
+            className="m-0 resize-none py-2 px-4 text-sm bg-white rounded outline-none transition-all duration-300 border border-solid border-gray-300 focus:ring-0 focus:outline-none focus:border-gray-500 hover:border-gray-500 data-[invalid=true]:text-red-400 data-[invalid=true]:border-red-400"
+            name={name}
+            id={name}
+            {...rest}
+          />
+        </Form.Control>
 
-      {validations &&
-        Object.entries(validations).map(([match, message]) => (
-          <Form.Message
-            className="text-xs text-red-400"
-            key={match}
-            match={match as Form.ValidityMatcher}
-          >
-            {message}
-          </Form.Message>
-        ))}
+        {validations &&
+          Object.entries(validations).map(([match, message]) => (
+            <Form.Message
+              className="text-xs text-red-400"
+              key={match}
+              match={match as Form.ValidityMatcher}
+            >
+              {message}
+            </Form.Message>
+          ))}
+      </fieldset>
     </Form.Field>
   );
-}
+});
--- src/components/organisms/create-invoice-dialog/CreateInvoiceDialog.tsx
+++ src/components/organisms/create-invoice-dialog/CreateInvoiceDialog.tsx
@@ -1,39 +1,38 @@
 import { Dispatch, FormEventHandler, SetStateAction, useState } from "react";
 import { Root as Form } from "@radix-ui/react-form";
 
-import { Invoice } from "lib/types";
+import { Invoice, Customer } from "lib/types";
 
 import { useInvoices } from "hooks/useInvoices";
 
 import { Dialog } from "components/atoms/dialog/Dialog";
-import { Input } from "components/atoms/input/Input";
 import { Button } from "components/atoms/button/Button";
+import { Select } from "components/atoms/select/Select";
+import { useCustomers } from "hooks/useCustomers";
+import { TextArea } from "components/atoms/textarea/TextArea";
 
 interface Props {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const EMPTY_INVOICE: Omit<Invoice, "id"> = {
-  description: "",
-  customer: {
-    id: "",
-    email: "",
-    name: "",
-    phone: "",
-  },
-};
-
 export function CreateInvoiceDialog({ setIsOpen }: Props) {
-  const { create } = useInvoices();
+  const invoices = useInvoices();
+  const customers = useCustomers();
 
-  const [invoice, setInvoice] = useState<Omit<Invoice, "id">>(EMPTY_INVOICE);
+  const [invoice, setInvoice] = useState<Omit<Invoice, "id">>({
+    description: "",
+    customer: customers.data[0],
+  });
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
 
     try {
-      await create([invoice]);
-      setInvoice(EMPTY_INVOICE);
+      await invoices.create([invoice]);
+      setInvoice({
+        description: "",
+        customer: customers.data[0],
+      });
       setIsOpen(false);
     } catch (error: unknown) {
       console.error(error);
@@ -46,21 +45,35 @@ export function CreateInvoiceDialog({ setIsOpen }: Props) {
       description="Add a new invoice to your database"
     >
       <Form className="mt-2 flex flex-col gap-2" onSubmit={handleSubmit}>
-        <Input
+        <TextArea
           label="Description"
           name="description"
-          type="text"
           value={invoice.description}
           onChange={(e) =>
             setInvoice((curr) => ({
               ...curr,
-              name: e.target.value,
+              description: e.target.value,
             }))
           }
           required
           validations={{
             valueMissing: "This field is required",
           }}
+        />
+
+        <Select
+          label="Customer"
+          name="customer"
+          options={customers.data.map((customer) => ({
+            label: customer.name,
+            value: customer.id,
+          }))}
+          onChange={(id) =>
+            setInvoice((curr) => ({
+              ...curr,
+              customer: customers.data.find((c) => c.id === id) as Customer,
+            }))
+          }
         />
 
         <Button size="lg" className="w-fit min-w-[100px] mt-4 ml-auto">
