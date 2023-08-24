a6818a8d9925fc7996dd5bf6d9024f86c2430881
Alba Ongaro <alba.ongaro@reprisk.com>
2023-08-24 16:41:19 +0200

    feat: added calendar heading

--- src/components/organisms/calendar/Calendar.tsx
+++ src/components/organisms/calendar/Calendar.tsx
@@ -7,8 +7,15 @@ import {
   isToday,
   format,
 } from "date-fns";
+import * as Menu from "@radix-ui/react-navigation-menu";
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "@radix-ui/react-icons";
 
 import { twMerge } from "lib/utils/twMerge";
+import { Button } from "components/atoms/button/Button";
 
 type Day = {
   date: string;
@@ -27,6 +34,29 @@ type Day = {
 interface Props {
   className?: string;
 }
+
+const views = [
+  {
+    id: "day",
+    label: "Day view",
+    disabled: true,
+  },
+  {
+    id: "week",
+    label: "Week view",
+    disabled: false,
+  },
+  {
+    id: "month",
+    label: "Month view",
+    disabled: true,
+  },
+  {
+    id: "year",
+    label: "Year view",
+    disabled: true,
+  },
+];
 
 export function Calendar({ className }: Props) {
   const days = useMemo<Day[]>(() => {
@@ -63,6 +93,69 @@ export function Calendar({ className }: Props) {
 
   return (
     <div className={twMerge("lg:flex lg:h-full lg:flex-col", className)}>
+      <header className="flex items-center justify-between border-b border-gray-200 py-6 lg:flex-none">
+        <h1 className="text-base font-semibold leading-6 text-gray-900">
+          <time dateTime="2022-01">{format(new Date(), "MMMM yyyy")}</time>
+        </h1>
+        <div className="flex items-center">
+          <div className="relative flex items-center rounded-md bg-white shadow-sm md:items-stretch">
+            <button
+              type="button"
+              className="flex h-9 w-12 items-center justify-center rounded-l-md border-y border-l border-gray-300 pr-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pr-0 md:hover:bg-gray-50"
+            >
+              <span className="sr-only">Previous month</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            <button
+              type="button"
+              className="hidden border-y border-gray-300 px-3.5 text-sm font-semibold text-gray-900 hover:bg-gray-50 focus:relative md:block"
+            >
+              Today
+            </button>
+            <span className="relative -mx-px h-5 w-px bg-gray-300 md:hidden" />
+            <button
+              type="button"
+              className="flex h-9 w-12 items-center justify-center rounded-r-md border-y border-r border-gray-300 pl-1 text-gray-400 hover:text-gray-500 focus:relative md:w-9 md:pl-0 md:hover:bg-gray-50"
+            >
+              <span className="sr-only">Next month</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="hidden md:ml-4 md:flex md:items-center">
+            <Menu.Root className="relative">
+              <Menu.List>
+                <Menu.Item>
+                  <Menu.Trigger asChild>
+                    <Button variant="tertiary" size="lg" className="flex gap-2">
+                      Month view
+                      <ChevronDownIcon
+                        className="-mr-1 h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </Button>
+                  </Menu.Trigger>
+                  <Menu.Content className="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                    {views.map((item) => (
+                      <Menu.Link asChild key={item.id}>
+                        <button
+                          disabled={item.disabled}
+                          className={twMerge(
+                            "block px-3 py-1 text-sm text-left leading-6 text-gray-900 w-full hover:bg-slate-50 disabled:text-gray-500 disabled:hover:bg-white",
+                          )}
+                        >
+                          {item.label}
+                        </button>
+                      </Menu.Link>
+                    ))}
+                  </Menu.Content>
+                </Menu.Item>
+              </Menu.List>
+            </Menu.Root>
+            <div className="ml-6 h-6 w-px bg-gray-300" />
+            <Button size="lg">Add event</Button>
+          </div>
+        </div>
+      </header>
       <div className="shadow ring-1 ring-black ring-opacity-5 lg:flex lg:flex-auto lg:flex-col">
         <div className="grid grid-cols-7 gap-px border-b border-gray-300 bg-gray-200 text-center text-xs font-semibold leading-6 text-gray-700 lg:flex-none">
           <div className="bg-white py-2">
