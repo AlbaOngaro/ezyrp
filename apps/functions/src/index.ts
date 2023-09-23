7bef4794d95534525ec2981cc128b614d87eb449
AlbaOngaro <alba_ongaro@hotmail.com>
2023-09-23 18:16:28 +0200

    fix: fixed prod host?

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -11,7 +11,7 @@ fastify.post("/deployment", deployment);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
