f0bf52b1952d59779dd6988516dda19996b5442e
AlbaOngaro <alba_ongaro@hotmail.com>
2024-06-20 09:51:25 +0200

    fix: fixed sidebar link active state

--- src/components/organisms/sidebar.tsx
+++ src/components/organisms/sidebar.tsx
@@ -47,7 +47,10 @@ export function Sidebar() {
                 className={cn(
                   "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
                   {
-                    "text-primary": router.asPath.startsWith(item.href),
+                    "text-primary":
+                      item.href === "/"
+                        ? router.asPath === "/"
+                        : router.asPath.startsWith(item.href),
                   },
                 )}
               >
