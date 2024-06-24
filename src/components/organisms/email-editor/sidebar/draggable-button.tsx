a584792c9bdb9089165f4f699cc89b0bb5f6841b
AlbaOngaro <alba_ongaro@hotmail.com>
2024-06-24 12:21:37 +0200

    perf: created draggable-wrapper to handle dragging logic

--- src/components/organisms/email-editor/sidebar/draggable-button.tsx
+++ src/components/organisms/email-editor/sidebar/draggable-button.tsx
@@ -1,9 +1,6 @@
-import { useDraggable } from "@dnd-kit/core";
-import { Transform, usePrevious } from "@dnd-kit/utilities";
-import { PropsWithChildren, useRef } from "react";
+import { PropsWithChildren } from "react";
 
-import { useGetSortableItems } from "../editable/hooks/useGetSortableItems";
-
+import { DraggableWrapper } from "./draggable-wrapper";
 import { ButtonElement } from "types/slate";
 
 type Props = PropsWithChildren<{
@@ -11,102 +8,41 @@ type Props = PropsWithChildren<{
 }>;
 
 const DRAGGABLE_BUTTON_ID = "draggable-button";
+const DATA = {
+  id: DRAGGABLE_BUTTON_ID,
+  type: "button",
+  href: "",
+  style: {
+    backgroundColor: "#5F51E8",
+    borderRadius: "3px",
+    color: "#fff",
+    fontSize: "16px",
+    textDecoration: "none",
+    textAlign: "center",
+    display: "block",
+    padding: "12px",
+  },
+  children: [{ text: "Button" }],
+} satisfies ButtonElement;
 
 export function DraggableButton({ disabled, children }: Props) {
-  const items = useGetSortableItems();
-  const elTransform = useRef<Transform | null>(null);
-  const { attributes, listeners, setNodeRef, setActivatorNodeRef, transform } =
-    useDraggable({
-      id: DRAGGABLE_BUTTON_ID,
-      disabled,
-      data: {
-        id: DRAGGABLE_BUTTON_ID,
-        type: "button",
-        href: "",
-        style: {
-          backgroundColor: "#5F51E8",
-          borderRadius: "3px",
-          color: "#fff",
-          fontSize: "16px",
-          textDecoration: "none",
-          textAlign: "center",
-          display: "block",
-          padding: "12px",
-        },
-        children: [{ text: "Button" }],
-      } satisfies ButtonElement,
-    });
-
-  const prevTransform = usePrevious(transform);
-
-  if (items.some((item) => item.id === DRAGGABLE_BUTTON_ID)) {
-    if (!elTransform.current) {
-      elTransform.current = transform;
-    }
-
-    return (
-      <button
-        ref={setActivatorNodeRef}
-        style={{
-          width: "100%",
-          backgroundColor: "#5F51E8",
-          borderRadius: "3px",
-          color: "#fff",
-          fontSize: "16px",
-          textDecoration: "none",
-          textAlign: "center",
-          display: "block",
-          padding: "12px",
-          zIndex: 1000,
-          transform: (() => {
-            if (elTransform.current && transform && prevTransform) {
-              const deltaX =
-                prevTransform.x === elTransform.current.x
-                  ? 0
-                  : transform.x - prevTransform.x;
-              const x = elTransform.current.x + deltaX;
-              const y = elTransform.current.y + transform.y;
-
-              elTransform.current = {
-                ...elTransform.current,
-                x,
-              };
-
-              return `translate3d(${x}px, ${y}px, 0)`;
-            }
-
-            return "unset";
-          })(),
-        }}
-        {...listeners}
-      >
-        {children}
-      </button>
-    );
-  }
-
   return (
-    <button
-      ref={setNodeRef}
-      style={{
-        width: "100%",
-        backgroundColor: "#5F51E8",
-        borderRadius: "3px",
-        color: "#fff",
-        fontSize: "16px",
-        textDecoration: "none",
-        textAlign: "center",
-        display: "block",
-        padding: "12px",
-        zIndex: 1000,
-        transform: transform
-          ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-          : "unset",
-      }}
-      {...listeners}
-      {...attributes}
-    >
-      {children}
-    </button>
+    <DraggableWrapper id={DRAGGABLE_BUTTON_ID} disabled={disabled} data={DATA}>
+      {({ transform, attributes, listeners, ref }) => (
+        <button
+          ref={ref}
+          style={{
+            ...DATA.style,
+            width: "100%",
+            zIndex: 1000,
+            transform,
+          }}
+          {...listeners}
+          {...attributes}
+        >
+          {children}
+        </button>
+      )}
+    </DraggableWrapper>
   );
 }
--- src/components/organisms/email-editor/sidebar/draggable-image.tsx
+++ src/components/organisms/email-editor/sidebar/draggable-image.tsx
@@ -1,11 +1,8 @@
-import { useDraggable } from "@dnd-kit/core";
 import { Image } from "lucide-react";
-import { Transform, usePrevious } from "@dnd-kit/utilities";
-import { useRef } from "react";
 
-import { useGetSortableItems } from "../editable/hooks/useGetSortableItems";
-import { ImgElement } from "types/slate";
+import { DraggableWrapper } from "./draggable-wrapper";
 import { Card } from "components/atoms/card";
+import { ImgElement } from "types/slate";
 
 type Props = {
   disabled?: boolean;
@@ -13,75 +10,30 @@ type Props = {
 };
 
 const DRAGGABLE_IMG_ID = "draggable-img";
+const DATA = {
+  id: DRAGGABLE_IMG_ID,
+  type: "img",
+  src: "/images/undraw_images_re_0kll.svg",
+  children: [{ text: "" }],
+} satisfies ImgElement;
 
 export function DraggableImg({ disabled }: Props) {
-  const items = useGetSortableItems();
-  const elTransform = useRef<Transform | null>(null);
-  const { attributes, listeners, setNodeRef, setActivatorNodeRef, transform } =
-    useDraggable({
-      id: DRAGGABLE_IMG_ID,
-      disabled,
-      data: {
-        id: DRAGGABLE_IMG_ID,
-        type: "img",
-        src: "/images/undraw_images_re_0kll.svg",
-        children: [{ text: "" }],
-      } satisfies ImgElement,
-    });
-  const prevTransform = usePrevious(transform);
-
-  if (items.some((item) => item.id === DRAGGABLE_IMG_ID)) {
-    if (!elTransform.current) {
-      elTransform.current = transform;
-    }
-
-    return (
-      <Card
-        className="p-2 aspect-square w-full max-w-[12rem] flex justify-center items-center"
-        ref={setActivatorNodeRef}
-        style={{
-          transform: (() => {
-            if (elTransform.current && transform && prevTransform) {
-              const deltaX =
-                prevTransform.x === elTransform.current.x
-                  ? 0
-                  : transform.x - prevTransform.x;
-              const x = elTransform.current.x + deltaX;
-              const y = elTransform.current.y + transform.y;
-
-              elTransform.current = {
-                ...elTransform.current,
-                x,
-              };
-
-              return `translate3d(${x}px, ${y}px, 0)`;
-            }
-
-            return "unset";
-          })(),
-        }}
-        {...listeners}
-      >
-        {/* eslint-disable-next-line */}
-        <Image className="w-12 h-12" />
-      </Card>
-    );
-  }
-
   return (
-    <Card
-      className="p-2 aspect-square w-full max-w-[12rem] flex justify-center items-center"
-      ref={setNodeRef}
-      style={{
-        transform: transform
-          ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-          : "unset",
-      }}
-      {...listeners}
-      {...attributes}
-    >
-      {/* eslint-disable-next-line */}
-      <Image className="w-12 h-12" />
-    </Card>
+    <DraggableWrapper id={DRAGGABLE_IMG_ID} disabled={disabled} data={DATA}>
+      {({ ref, transform, listeners, attributes }) => (
+        <Card
+          className="p-2 aspect-square w-full max-w-[12rem] flex justify-center items-center"
+          ref={ref}
+          style={{
+            transform,
+          }}
+          {...listeners}
+          {...attributes}
+        >
+          {/* eslint-disable-next-line */}
+          <Image className="w-12 h-12" />
+        </Card>
+      )}
+    </DraggableWrapper>
   );
 }
