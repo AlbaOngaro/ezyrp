43fe1019fd1be10ef8a69aef2c643db919e9c42f
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-03 00:13:04 +0200

    fix: fixed icon colors

--- src/components/organisms/flow-editor/components/nodes/utils/getIconForNode.tsx
+++ src/components/organisms/flow-editor/components/nodes/utils/getIconForNode.tsx
@@ -21,8 +21,8 @@ export function getIconForNode({ type, data, variant = "default" }: Props) {
   const className = cn({
     "h-6 w-6": variant === "ghost",
     "p-2 rounded-sm w-8 h-8": variant === "default",
-    "bg-orange-300": variant === "default" && type === "action",
-    "bg-green-300": variant === "default" && type === "trigger",
+    "bg-orange-300": variant === "default" && type === "trigger",
+    "bg-green-300": variant === "default" && type === "action",
   });
 
   if (type === "action") {
