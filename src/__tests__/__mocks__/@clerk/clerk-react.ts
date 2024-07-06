f04b37bcf3bc7bc7be71ddd126fc1342039d7952
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-07 01:17:30 +0200

    chore: fixed linting errors

--- src/__tests__/__mocks__/@clerk/clerk-react.ts
+++ src/__tests__/__mocks__/@clerk/clerk-react.ts
@@ -2,20 +2,20 @@ import { UserResource } from "@clerk/types";
 
 type UseUserReturn =
   | {
-    isLoaded: false;
-    isSignedIn: undefined;
-    user: undefined;
-  }
+      isLoaded: false;
+      isSignedIn: undefined;
+      user: undefined;
+    }
   | {
-    isLoaded: true;
-    isSignedIn: false;
-    user: null;
-  }
+      isLoaded: true;
+      isSignedIn: false;
+      user: null;
+    }
   | {
-    isLoaded: true;
-    isSignedIn: true;
-    user: UserResource;
-  };
+      isLoaded: true;
+      isSignedIn: true;
+      user: UserResource;
+    };
 
 export function useUser(): UseUserReturn {
   return {
