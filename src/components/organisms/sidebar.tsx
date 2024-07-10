774fdc356742d8547f8435a0445fda050e5c612c
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-11 00:45:47 +0200

    feat: added new logo and favico

--- src/components/organisms/sidebar.tsx
+++ src/components/organisms/sidebar.tsx
@@ -9,6 +9,7 @@ import {
   Settings,
   ReceiptText,
   Workflow,
+  Lightbulb,
 } from "lucide-react";
 import { useRouter } from "next/router";
 import { Button } from "components/atoms/button";
@@ -32,7 +33,7 @@ export function Sidebar() {
       <div className="flex h-full max-h-screen flex-col gap-2">
         <header className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
-            <Package2 className="h-6 w-6" />
+            <img src="/images/logo.svg" className="h-8 w-8" alt="logo" />
             <span className="">Ezyrp</span>
           </Link>
           <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
