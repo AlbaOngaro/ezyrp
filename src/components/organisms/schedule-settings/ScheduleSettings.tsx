e08486beac3eba9660f50aa68e84ab50e9d70c47
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-15 12:03:53 +0200

    chore: fixed build errors

--- src/components/organisms/schedule-settings/ScheduleSettings.tsx
+++ src/components/organisms/schedule-settings/ScheduleSettings.tsx
@@ -1,6 +1,7 @@
 import { Form } from "@radix-ui/react-form";
 import { format } from "date-fns";
 import { Controller, useForm } from "react-hook-form";
+import { forwardRef } from "react";
 import { useMutation } from "lib/hooks/useMutation";
 import { Container } from "components/atoms/container";
 import { Heading } from "components/atoms/heading";
@@ -26,146 +27,151 @@ const HOURS = Array.from({ length: 48 }, (_, i) => {
 
 const DAYS = ["M", "T", "W", "T", "F", "S", "S"];
 
-export function ScheduleSettings() {
-  const [loadSettings] = useLazyQuery(api.settings.get);
-  const updateSettings = useMutation(api.settings.update);
+export const ScheduleSettings = forwardRef<HTMLDivElement, unknown>(
+  function ScheduleSettings(_, ref) {
+    const [loadSettings] = useLazyQuery(api.settings.get);
+    const updateSettings = useMutation(api.settings.update);
 
-  const {
-    control,
-    handleSubmit,
-    formState: { isValid, isSubmitting },
-  } = useForm({
-    defaultValues: async () => {
-      const settings = await loadSettings();
+    const {
+      control,
+      handleSubmit,
+      formState: { isValid, isSubmitting },
+    } = useForm({
+      defaultValues: async () => {
+        const settings = await loadSettings();
 
-      if (!settings) {
+        if (!settings) {
+          return {
+            start: "09:00",
+            end: "17:00",
+            days: [],
+          };
+        }
+
         return {
-          start: "09:00",
-          end: "17:00",
-          days: [],
+          start: settings?.start || "09:00",
+          end: settings?.end || "17:00",
+          days: settings?.days || [],
         };
-      }
+      },
+    });
 
-      return {
-        start: settings?.start || "09:00",
-        end: settings?.end || "17:00",
-        days: settings?.days || [],
-      };
-    },
-  });
-
-  return (
-    <Container className="grid grid-cols-[1fr_2fr] py-12 gap-x-4 max-w-none h-[calc(100vh_-_122px)] overflow-y-scroll">
-      <Heading
-        title="Working hours"
-        description="Effortlessly tailor your working time settings to optimize productivity and work-life balance"
-      />
-
-      <Form
-        className="flex flex-col gap-4"
-        onSubmit={handleSubmit(async (updateSettingsInput) => {
-          await updateSettings(updateSettingsInput);
-        }, console.error)}
+    return (
+      <Container
+        ref={ref}
+        className="grid grid-cols-[1fr_2fr] py-12 gap-x-4 max-w-none h-[calc(100vh_-_122px)] overflow-y-scroll"
       >
-        <Controller
-          control={control}
-          name="start"
-          rules={{
-            required: true,
-          }}
-          render={({ field: { value, onChange } }) => {
-            const selected = HOURS.find((hour) => hour.value === value);
-
-            return (
-              <Select
-                name="start"
-                label="Work day starts at"
-                value={selected}
-                options={HOURS}
-                defaultValue={{
-                  label: "09:00",
-                  value: "09:00",
-                }}
-                onChange={(option) => {
-                  if (option) {
-                    onChange(option.value);
-                  }
-                }}
-              />
-            );
-          }}
+        <Heading
+          title="Working hours"
+          description="Effortlessly tailor your working time settings to optimize productivity and work-life balance"
         />
 
-        <Controller
-          control={control}
-          name="end"
-          rules={{
-            required: true,
-          }}
-          render={({ field: { value, onChange } }) => {
-            const selected = HOURS.find((hour) => hour.value === value);
-            return (
-              <Select
-                name="end"
-                label="Work day ends at"
-                options={HOURS}
-                value={selected}
-                defaultValue={{
-                  label: "17:00",
-                  value: "17:00",
-                }}
-                onChange={(option) => {
-                  if (option) {
-                    onChange(option.value);
-                  }
-                }}
-              />
-            );
-          }}
-        />
-
-        <Controller
-          control={control}
-          name="days"
-          rules={{
-            required: true,
-          }}
-          render={({ field: { value = [], onChange } }) => (
-            <div className="flex flex-col gap-2">
-              <label className="flex flex-col text-sm font-bold text-gray-800">
-                Work week
-              </label>
-              <fieldset className="flex flex-row w-fit rounded-md border border-solid border-gray-300 overflow-hidden">
-                {DAYS.map((day, i) => (
-                  <input
-                    key={i}
-                    type="checkbox"
-                    value={day}
-                    checked={value?.includes(i)}
-                    onChange={() => {
-                      if (value?.includes(i)) {
-                        onChange(value.filter((v) => v !== i));
-                        return;
-                      }
-
-                      onChange([...(value || []), i]);
-                    }}
-                    className="after:content-[attr(value)] after:absolute after:w-full after:h-full after:flex after:justify-center after:items-center cursor-pointer hover:[&:not(:checked)]:after:bg-black hover:[&:not(:checked)]:after:text-white text-black checked:after:bg-black checked:after:text-white appearance-none relative w-8 h-8 flex items-center justify-center !outline-none border-l border-0 border-solid border-gray-300 [&:first-child]:border-none [&:first-child]:rounded-l-md [&:last-child]:rounded-r-md !ring-0 !shadow-none"
-                  />
-                ))}
-              </fieldset>
-            </div>
-          )}
-        />
-
-        <Button
-          className="px-6 w-fit"
-          disabled={!isValid}
-          loading={isSubmitting}
+        <Form
+          className="flex flex-col gap-4"
+          onSubmit={handleSubmit(async (updateSettingsInput) => {
+            await updateSettings(updateSettingsInput);
+          }, console.error)}
         >
-          Save
-        </Button>
-      </Form>
-    </Container>
-  );
-}
+          <Controller
+            control={control}
+            name="start"
+            rules={{
+              required: true,
+            }}
+            render={({ field: { value, onChange } }) => {
+              const selected = HOURS.find((hour) => hour.value === value);
+
+              return (
+                <Select
+                  name="start"
+                  label="Work day starts at"
+                  value={selected}
+                  options={HOURS}
+                  defaultValue={{
+                    label: "09:00",
+                    value: "09:00",
+                  }}
+                  onChange={(option) => {
+                    if (option) {
+                      onChange(option.value);
+                    }
+                  }}
+                />
+              );
+            }}
+          />
+
+          <Controller
+            control={control}
+            name="end"
+            rules={{
+              required: true,
+            }}
+            render={({ field: { value, onChange } }) => {
+              const selected = HOURS.find((hour) => hour.value === value);
+              return (
+                <Select
+                  name="end"
+                  label="Work day ends at"
+                  options={HOURS}
+                  value={selected}
+                  defaultValue={{
+                    label: "17:00",
+                    value: "17:00",
+                  }}
+                  onChange={(option) => {
+                    if (option) {
+                      onChange(option.value);
+                    }
+                  }}
+                />
+              );
+            }}
+          />
+
+          <Controller
+            control={control}
+            name="days"
+            rules={{
+              required: true,
+            }}
+            render={({ field: { value = [], onChange } }) => (
+              <div className="flex flex-col gap-2">
+                <label className="flex flex-col text-sm font-bold text-gray-800">
+                  Work week
+                </label>
+                <fieldset className="flex flex-row w-fit rounded-md border border-solid border-gray-300 overflow-hidden">
+                  {DAYS.map((day, i) => (
+                    <input
+                      key={i}
+                      type="checkbox"
+                      value={day}
+                      checked={value?.includes(i)}
+                      onChange={() => {
+                        if (value?.includes(i)) {
+                          onChange(value.filter((v) => v !== i));
+                          return;
+                        }
+
+                        onChange([...(value || []), i]);
+                      }}
+                      className="after:content-[attr(value)] after:absolute after:w-full after:h-full after:flex after:justify-center after:items-center cursor-pointer hover:[&:not(:checked)]:after:bg-black hover:[&:not(:checked)]:after:text-white text-black checked:after:bg-black checked:after:text-white appearance-none relative w-8 h-8 flex items-center justify-center !outline-none border-l border-0 border-solid border-gray-300 [&:first-child]:border-none [&:first-child]:rounded-l-md [&:last-child]:rounded-r-md !ring-0 !shadow-none"
+                    />
+                  ))}
+                </fieldset>
+              </div>
+            )}
+          />
+
+          <Button
+            className="px-6 w-fit"
+            disabled={!isValid}
+            loading={isSubmitting}
+          >
+            Save
+          </Button>
+        </Form>
+      </Container>
+    );
+  },
+);
--- src/components/organisms/team-settings/index.tsx
+++ src/components/organisms/team-settings/index.tsx
@@ -1,6 +1,6 @@
 import { FormProvider, UseFormHandleSubmit, useForm } from "react-hook-form";
 import { useOrganization } from "@clerk/clerk-react";
-import { useState } from "react";
+import { forwardRef, useState } from "react";
 import { toast } from "sonner";
 
 import { useAction } from "convex/react";
@@ -28,207 +28,214 @@ import {
   TooltipTrigger,
 } from "components/atoms/tooltip";
 
-export function TeamSettings() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [isInviteMemberOpen, setIsInviteMemberOpen] = useState(false);
-  const { isLoaded, organization, invitations, memberships } = useOrganization({
-    invitations: {
-      pageSize: 5,
-      keepPreviousData: true,
-    },
-    memberships: {
-      pageSize: 5,
-      keepPreviousData: true,
-    },
-  });
+export const TeamSettings = forwardRef<HTMLDivElement, unknown>(
+  function TeamSettings(_, ref) {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isInviteMemberOpen, setIsInviteMemberOpen] = useState(false);
+    const { isLoaded, organization, invitations, memberships } =
+      useOrganization({
+        invitations: {
+          pageSize: 5,
+          keepPreviousData: true,
+        },
+        memberships: {
+          pageSize: 5,
+          keepPreviousData: true,
+        },
+      });
 
-  const { handleSubmit, ...methods } = useForm<InviteMemberFormValue>({
-    defaultValues: {
-      email: "",
-      role: "org:member",
-    },
-  });
+    const { handleSubmit, ...methods } = useForm<InviteMemberFormValue>({
+      defaultValues: {
+        email: "",
+        role: "org:member",
+      },
+    });
 
-  const invite = useAction(api.workspaces.invite);
+    const invite = useAction(api.workspaces.invite);
 
-  const handleSubmitWrapper: UseFormHandleSubmit<InviteMemberFormValue> = (
-    onSucces,
-    onError,
-  ) =>
-    handleSubmit(async ({ email, role }) => {
-      if (!!organization) {
-        try {
-          await invite({
-            email,
-            role,
-          });
-
-          onSucces({ email, role });
-
-          if (!!invitations && typeof invitations.revalidate === "function") {
-            await invitations.revalidate();
-          }
-        } catch (error: unknown) {
-          if (error instanceof Error) {
-            toast.error("Failed to invite member", {
-              description: error.message,
+    const handleSubmitWrapper: UseFormHandleSubmit<InviteMemberFormValue> = (
+      onSucces,
+      onError,
+    ) =>
+      handleSubmit(async ({ email, role }) => {
+        if (!!organization) {
+          try {
+            await invite({
+              email,
+              role,
             });
-          } else {
-            toast.error("Failed to invite member");
+
+            onSucces({ email, role });
+
+            if (!!invitations && typeof invitations.revalidate === "function") {
+              await invitations.revalidate();
+            }
+          } catch (error: unknown) {
+            if (error instanceof Error) {
+              toast.error("Failed to invite member", {
+                description: error.message,
+              });
+            } else {
+              toast.error("Failed to invite member");
+            }
+          } finally {
+            setIsInviteMemberOpen(false);
           }
-        } finally {
-          setIsInviteMemberOpen(false);
         }
-      }
-    }, onError);
+      }, onError);
 
-  if (!isLoaded || !organization) {
+    if (!isLoaded || !organization) {
+      return (
+        <main className="h-screen w-screen flex justify-center items-center">
+          <Loader />
+        </main>
+      );
+    }
+
+    const members = [
+      ...(memberships?.data || []).map((membership) => ({
+        type: "member",
+        _id: membership.id,
+        role: membership.role,
+        email: membership.publicUserData.identifier,
+      })),
+      ...(invitations?.data || []).map((invitation) => ({
+        type: "invitation",
+        role: invitation.role,
+        _id: invitation.id,
+        email: invitation.emailAddress,
+      })),
+    ];
+
     return (
-      <main className="h-screen w-screen flex justify-center items-center">
-        <Loader />
-      </main>
-    );
-  }
-
-  const members = [
-    ...(memberships?.data || []).map((membership) => ({
-      type: "member",
-      _id: membership.id,
-      role: membership.role,
-      email: membership.publicUserData.identifier,
-    })),
-    ...(invitations?.data || []).map((invitation) => ({
-      type: "invitation",
-      role: invitation.role,
-      _id: invitation.id,
-      email: invitation.emailAddress,
-    })),
-  ];
-
-  return (
-    <>
-      <Container as="section" className="py-10 sm:flex sm:items-center">
-        <Heading
-          title="Team memebers"
-          description="A list of all the team members who have been added to your workspace"
-        />
-
-        <ModalRoot open={isInviteMemberOpen}>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  disabled={members.length === 5}
-                  onClick={() => setIsInviteMemberOpen(true)}
-                  data-testid="settings-team__invite-button"
-                >
-                  Invite team member
-                </Button>
-              </TooltipTrigger>
-              {members.length === 5 && (
-                <TooltipContent>
-                  You can only invite up to 5 members
-                </TooltipContent>
-              )}
-            </Tooltip>
-          </TooltipProvider>
-
-          <Modal>
-            <FormProvider {...methods} handleSubmit={handleSubmitWrapper}>
-              <CreateInviteForm />
-            </FormProvider>
-          </Modal>
-        </ModalRoot>
-      </Container>
-
-      <Container as="section">
-        <Card>
-          <Table
-            loading={isLoading}
-            rows={members}
-            withMultiSelect
-            columns={[
-              {
-                id: "id",
-                field: "_id",
-                headerName: " ",
-                width: convertRemToPx(1),
-                render: ({ email }) => (
-                  <Avatar className="block" seed={email} />
-                ),
-              },
-              {
-                id: "email",
-                field: "email",
-                sortable: true,
-                headerName: "Email",
-              },
-              {
-                id: "type",
-                field: "type",
-                headerName: "Type",
-                sortable: true,
-              },
-              {
-                id: "role",
-                field: "role",
-                headerName: "Role",
-                sortable: true,
-                render: ({ role }) => <Badge variant="outline">{role}</Badge>,
-              },
-            ]}
-            contextMenuItems={[
-              {
-                type: "item",
-                label: "Remove",
-                onClick: ({ type, _id }) =>
-                  dialogs.warning({
-                    title: `Do you really want to remove this ${type}?`,
-                    description: "This action cannot be undone!",
-                    onConfirm: async () => {
-                      try {
-                        setIsLoading(true);
-
-                        switch (type) {
-                          case "member": {
-                            const membership = memberships?.data?.find(
-                              (membership) => membership.id === _id,
-                            );
-
-                            if (!!membership) {
-                              await membership.destroy();
-                              if (memberships?.revalidate) {
-                                await memberships?.revalidate();
-                              }
-                            }
-                            break;
-                          }
-                          case "invitation": {
-                            const invitation = invitations?.data?.find(
-                              (invitation) => invitation.id === _id,
-                            );
-
-                            if (!!invitation) {
-                              await invitation.revoke();
-                              if (invitations?.revalidate) {
-                                await invitations?.revalidate();
-                              }
-                            }
-                            break;
-                          }
-                        }
-                      } catch (error) {
-                        console.error(error);
-                      } finally {
-                        setIsLoading(false);
-                      }
-                    },
-                  }),
-              },
-            ]}
+      <>
+        <Container
+          as="section"
+          className="py-10 sm:flex sm:items-center"
+          ref={ref}
+        >
+          <Heading
+            title="Team memebers"
+            description="A list of all the team members who have been added to your workspace"
           />
-        </Card>
-      </Container>
-    </>
-  );
-}
+
+          <ModalRoot open={isInviteMemberOpen}>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    disabled={members.length === 5}
+                    onClick={() => setIsInviteMemberOpen(true)}
+                    data-testid="settings-team__invite-button"
+                  >
+                    Invite team member
+                  </Button>
+                </TooltipTrigger>
+                {members.length === 5 && (
+                  <TooltipContent>
+                    You can only invite up to 5 members
+                  </TooltipContent>
+                )}
+              </Tooltip>
+            </TooltipProvider>
+
+            <Modal>
+              <FormProvider {...methods} handleSubmit={handleSubmitWrapper}>
+                <CreateInviteForm />
+              </FormProvider>
+            </Modal>
+          </ModalRoot>
+        </Container>
+
+        <Container as="section">
+          <Card>
+            <Table
+              loading={isLoading}
+              rows={members}
+              withMultiSelect
+              columns={[
+                {
+                  id: "id",
+                  field: "_id",
+                  headerName: " ",
+                  width: convertRemToPx(1),
+                  render: ({ email }) => (
+                    <Avatar className="block" seed={email} />
+                  ),
+                },
+                {
+                  id: "email",
+                  field: "email",
+                  sortable: true,
+                  headerName: "Email",
+                },
+                {
+                  id: "type",
+                  field: "type",
+                  headerName: "Type",
+                  sortable: true,
+                },
+                {
+                  id: "role",
+                  field: "role",
+                  headerName: "Role",
+                  sortable: true,
+                  render: ({ role }) => <Badge variant="outline">{role}</Badge>,
+                },
+              ]}
+              contextMenuItems={[
+                {
+                  type: "item",
+                  label: "Remove",
+                  onClick: ({ type, _id }) =>
+                    dialogs.warning({
+                      title: `Do you really want to remove this ${type}?`,
+                      description: "This action cannot be undone!",
+                      onConfirm: async () => {
+                        try {
+                          setIsLoading(true);
+
+                          switch (type) {
+                            case "member": {
+                              const membership = memberships?.data?.find(
+                                (membership) => membership.id === _id,
+                              );
+
+                              if (!!membership) {
+                                await membership.destroy();
+                                if (memberships?.revalidate) {
+                                  await memberships?.revalidate();
+                                }
+                              }
+                              break;
+                            }
+                            case "invitation": {
+                              const invitation = invitations?.data?.find(
+                                (invitation) => invitation.id === _id,
+                              );
+
+                              if (!!invitation) {
+                                await invitation.revoke();
+                                if (invitations?.revalidate) {
+                                  await invitations?.revalidate();
+                                }
+                              }
+                              break;
+                            }
+                          }
+                        } catch (error) {
+                          console.error(error);
+                        } finally {
+                          setIsLoading(false);
+                        }
+                      },
+                    }),
+                },
+              ]}
+            />
+          </Card>
+        </Container>
+      </>
+    );
+  },
+);
