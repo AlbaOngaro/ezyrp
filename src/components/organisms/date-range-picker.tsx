cd697c691df300027a8a8f505e3ed29435e57683
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-17 19:22:50 +0200

    feat: added range picker in homepage

---
+++ src/components/organisms/date-range-picker.tsx
@@ -0,0 +1,80 @@
+import React, { HTMLAttributes, useState } from "react";
+import { addDays, format } from "date-fns";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { DateRange } from "react-day-picker";
+
+import { Button } from "components/atoms/button";
+import { Calendar } from "components/atoms/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "components/atoms/popover";
+import { cn } from "lib/utils/cn";
+
+type Props = Omit<HTMLAttributes<HTMLDivElement>, "onChange"> & {
+  range?: DateRange;
+  onChange?: (range: DateRange | undefined) => void;
+};
+
+const defaultRange = {
+  from: new Date(2022, 0, 20),
+  to: addDays(new Date(2022, 0, 20), 20),
+};
+
+export function DateRangePicker({
+  className,
+  onChange,
+  range = defaultRange,
+}: Props) {
+  const [date, setDate] = useState<DateRange>(range);
+
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[300px] justify-start text-left font-normal",
+              !date && "text-muted-foreground",
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={(date) => {
+              if (date) {
+                setDate(date);
+              }
+
+              if (typeof onChange == "function") {
+                onChange(date);
+              }
+            }}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -1,23 +1,26 @@
-import { ReactElement } from "react";
-import { formatISO, subDays } from "date-fns";
+import { ReactElement, useState } from "react";
+import { formatISO, parseISO, subDays } from "date-fns";
 
 import { SidebarLayout } from "components/layouts/sidebar/SidebarLayout";
-
 import { Container } from "components/atoms/container";
-import { Card } from "components/atoms/card";
 import { api } from "convex/_generated/api";
 import { useQuery } from "lib/hooks/useQuery";
 import { Loader } from "components/atoms/loader";
 import { StatCard, Stats } from "components/organisms/stat-card";
 import { Heading } from "components/atoms/heading";
+import { DateRangePicker } from "components/organisms/date-range-picker";
 
 export function HomePage() {
-  const start = formatISO(subDays(new Date(), 7), {
-    representation: "date",
-  });
-  const end = formatISO(new Date(), {
-    representation: "date",
-  });
+  const [start, setStart] = useState(
+    formatISO(subDays(new Date(), 7), {
+      representation: "date",
+    }),
+  );
+  const [end, setEnd] = useState(
+    formatISO(new Date(), {
+      representation: "date",
+    }),
+  );
 
   const { data, status } = useQuery(api.stats.get, {
     range: {
@@ -26,36 +29,41 @@ export function HomePage() {
     },
   });
 
-  if (status === "pending") {
-    return (
-      <Container as="section">
-        <Loader />
-      </Container>
-    );
-  }
-
-  if (status === "error") {
-    return (
-      <Container as="section">
-        <Card>
-          <p>Failed to load data</p>
-        </Card>
-      </Container>
-    );
-  }
-
   return (
     <>
       <Container as="section" className="py-10 sm:flex sm:items-center">
         <Heading title="Dashboard" description="An overview of your team" />
+
+        <DateRangePicker
+          range={{
+            from: parseISO(start),
+            to: parseISO(end),
+          }}
+          onChange={(range) => {
+            const from = range?.from;
+            const to = range?.to;
+
+            if (!!from) {
+              setStart(formatISO(from, { representation: "date" }));
+            }
+
+            if (!!to) {
+              setEnd(formatISO(to, { representation: "date" }));
+            }
+          }}
+        />
       </Container>
 
       <Container as="section">
-        <div className="grid grid-cols-4 gap-x-4">
-          {Object.entries(data).map(([key, value]) => (
-            <StatCard key={key} type={key as keyof Stats} value={value} />
-          ))}
-        </div>
+        {status === "success" ? (
+          <div className="grid grid-cols-4 gap-x-4">
+            {Object.entries(data).map(([key, value]) => (
+              <StatCard key={key} type={key as keyof Stats} value={value} />
+            ))}
+          </div>
+        ) : (
+          <Loader />
+        )}
       </Container>
     </>
   );
