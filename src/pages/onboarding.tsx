23b5c1fc438dd81049f16d984832641f86a75f63
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-27 22:34:50 +0200

    perf: initial onboarding re-styling

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -4,13 +4,19 @@ import { FormEventHandler, useState } from "react";
 import { useAction } from "convex/react";
 import * as RadioGroup from "@radix-ui/react-radio-group";
 import { Button } from "components/atoms/button";
-import { Card } from "components/atoms/card";
 import { Input } from "components/atoms/input";
 import { api } from "convex/_generated/api";
 import { Heading } from "components/atoms/heading";
 import { Badge } from "components/atoms/badge";
 import { cn } from "lib/utils/cn";
 import { Paragraph } from "components/atoms/typography";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "components/atoms/accordion";
 
 enum Step {
   Workspace,
@@ -54,13 +60,18 @@ export default function OnboardingPage() {
   };
 
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
-      <Card className="w-2/3 px-6 pb-6 pt-4 grid grid-cols-[1fr_2fr]">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row items-center gap-4">
+    <main className="w-screen h-screen grid grid-cols-4">
+      <aside className="bg-gray-100 h-full p-4">
+        <span className="flex items-center gap-2 font-semibold text-xl mb-20">
+          <img src="/images/logo.svg" className="h-10 w-10" alt="logo" />
+          Ezyrp
+        </span>
+
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-row items-start gap-3">
             <Badge
               className={cn(
-                "w-10 h-10 flex justify-center items-center bg-black",
+                "w-8 h-8 p-0 flex justify-center items-center bg-black",
               )}
             >
               {step === Step.Workspace ? "1" : "✓"}
@@ -72,10 +83,10 @@ export default function OnboardingPage() {
             />
           </div>
 
-          <div className="flex flex-row items-center gap-4">
+          <div className="flex flex-row items-start gap-3">
             <Badge
               className={cn(
-                "w-10 h-10 flex justify-center items-center bg-gray-400",
+                "w-8 h-8 p-0 flex justify-center items-center bg-gray-400",
                 {
                   "bg-black": step === Step.Plan || step === Step.Summary,
                 },
@@ -86,14 +97,16 @@ export default function OnboardingPage() {
             <Heading
               title="Step 2"
               description="Setup your billing plan"
-              className="[&_p]:mt-0"
+              className={cn("[&_p]:mt-0", {
+                "text-red-300": step < Step.Summary,
+              })}
             />
           </div>
 
-          <div className="flex flex-row items-center gap-4">
+          <div className="flex flex-row items-start gap-3">
             <Badge
               className={cn(
-                "w-10 h-10 flex justify-center items-center bg-gray-400",
+                "w-8 h-8 p-0 flex justify-center items-center bg-gray-400",
                 {
                   "bg-black": step === Step.Summary,
                 },
@@ -108,7 +121,13 @@ export default function OnboardingPage() {
             />
           </div>
         </div>
-        <Form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+      </aside>
+
+      <section className="col-span-3 h-full grid grid-rows-[1fr_1rem] px-4 py-8">
+        <Form
+          className="flex flex-col gap-4 w-full max-w-[400px] self-center justify-self-center"
+          onSubmit={handleSubmit}
+        >
           {(() => {
             switch (step) {
               case Step.Workspace: {
@@ -116,7 +135,6 @@ export default function OnboardingPage() {
                   <Input
                     value={name}
                     label="Workspace name"
-                    description="This should match the name of your organization or team. You can change it later."
                     name="workspace"
                     placeholder="Acme"
                     onChange={(e) => setName(e.target.value)}
@@ -129,35 +147,75 @@ export default function OnboardingPage() {
               case Step.Plan: {
                 return (
                   <RadioGroup.Root
-                    className="grid grid-cols-2 gap-2"
+                    className="grid gap-2"
                     defaultValue={plan}
                     onValueChange={(value) => setPlan(value as "free" | "pro")}
                   >
-                    <article className="flex flex-col gap-4">
-                      <Heading
-                        title="Free"
-                        description="Get started with the basics"
-                      />
-                      <RadioGroup.Item value="free" asChild>
-                        <Button
-                          variant="outline"
-                          className="w-full data-[state=checked]:bg-black data-[state=checked]:text-white"
-                        >
-                          Chose
-                        </Button>
-                      </RadioGroup.Item>
-                    </article>
-                    <article className="flex flex-col gap-4">
-                      <Heading title="Pro" description="Unlock more features" />
-                      <RadioGroup.Item value="pro" asChild>
-                        <Button
-                          variant="outline"
-                          className="w-full data-[state=checked]:bg-black data-[state=checked]:text-white"
-                        >
-                          Chose
-                        </Button>
-                      </RadioGroup.Item>
-                    </article>
+                    <Accordion type="single" collapsible defaultValue={plan}>
+                      <AccordionItem value="free">
+                        <AccordionTrigger className="font-semibold">
+                          Free
+                        </AccordionTrigger>
+                        <AccordionContent>
+                          <Paragraph className="mb-2">
+                            Get started with the basic features:
+                            <ul className="list-outside list-disc pl-4">
+                              <li className="list-item">
+                                Up to 5 team members
+                              </li>
+                              <li className="list-item">Unlimited customers</li>
+                              <li className="list-item">
+                                Inventory management
+                              </li>
+                              <li className="list-item">Invoices management</li>
+                              <li className="list-item">Schedule management</li>
+                              <li className="list-item">
+                                Up to 5 custom event types
+                              </li>
+                            </ul>
+                          </Paragraph>
+                          <RadioGroup.Item value="free" asChild>
+                            <Button
+                              variant="outline"
+                              className="w-full data-[state=checked]:bg-black data-[state=checked]:text-white"
+                            >
+                              Chose
+                            </Button>
+                          </RadioGroup.Item>
+                        </AccordionContent>
+                      </AccordionItem>
+
+                      <AccordionItem value="pro">
+                        <AccordionTrigger className="font-semibold">
+                          Pro
+                        </AccordionTrigger>
+                        <AccordionContent>
+                          <Paragraph className="mb-2">
+                            Unlock all the features with the pro plan:
+                            <ul className="list-outside list-disc pl-4">
+                              <li className="list-item">
+                                All free plan features
+                              </li>
+                              <li className="list-item">
+                                Unlimited event types
+                              </li>
+                              <li className="list-item">
+                                Custom email templates
+                              </li>
+                              <li className="list-item">Custom workflows</li>
+                            </ul>
+                          </Paragraph>
+                          <RadioGroup.Item value="pro" asChild>
+                            <Button
+                              variant="outline"
+                              className="w-full data-[state=checked]:bg-black data-[state=checked]:text-white"
+                            >
+                              Chose
+                            </Button>
+                          </RadioGroup.Item>
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
                   </RadioGroup.Root>
                 );
               }
@@ -184,53 +242,62 @@ export default function OnboardingPage() {
             }
           })()}
 
-          <footer className="mt-auto ml-auto flex flex-row justify-end gap-2">
-            {step !== Step.Workspace && (
-              <Button
-                variant="outline"
-                className="w-28"
-                onClick={() =>
-                  setStep((curr) => {
-                    switch (curr) {
-                      case Step.Plan:
-                        return Step.Workspace;
-                      case Step.Summary:
-                        return Step.Plan;
-                      default:
-                        return curr;
-                    }
-                  })
-                }
-              >
-                Previous
-              </Button>
-            )}
-            <Button
-              loading={isLoading}
-              className="w-28"
-              type="submit"
-              onClick={(e) =>
-                setStep((curr) => {
-                  switch (curr) {
-                    case Step.Workspace: {
-                      e.preventDefault();
-                      return Step.Plan;
-                    }
-                    case Step.Plan: {
-                      e.preventDefault();
-                      return Step.Summary;
-                    }
-                    default:
-                      return curr;
+          <Button
+            loading={isLoading}
+            type="submit"
+            onClick={(e) =>
+              setStep((curr) => {
+                switch (curr) {
+                  case Step.Workspace: {
+                    e.preventDefault();
+                    return Step.Plan;
                   }
-                })
-              }
-            >
-              {step === Step.Summary ? "Confirm" : "Next"}
-            </Button>
-          </footer>
+                  case Step.Plan: {
+                    e.preventDefault();
+                    return Step.Summary;
+                  }
+                  default:
+                    return curr;
+                }
+              })
+            }
+          >
+            {step === Step.Summary ? "Confirm" : "Continue"}
+          </Button>
         </Form>
-      </Card>
+
+        <nav>
+          <ul className="flex flex-row gap-2 justify-center">
+            <li
+              className={cn(
+                "w-2 h-2 transition-all rounded-full bg-gray-300 block hover:bg-gray-400 hover:cursor-pointer",
+                {
+                  "w-6 bg-blue-600 hover:bg-blue-600": step === Step.Workspace,
+                },
+              )}
+              onClick={() => setStep(Step.Workspace)}
+            />
+            <li
+              className={cn(
+                "w-2 h-2 transition-all rounded-full bg-gray-300 block hover:bg-gray-400 hover:cursor-pointer",
+                {
+                  "w-6 bg-blue-600 hover:bg-blue-600": step === Step.Plan,
+                },
+              )}
+              onClick={() => setStep(Step.Plan)}
+            />
+            <li
+              className={cn(
+                "w-2 h-2 transition-all rounded-full bg-gray-300 block hover:bg-gray-400 hover:cursor-pointer",
+                {
+                  "w-6 bg-blue-600 hover:bg-blue-600": step === Step.Summary,
+                },
+              )}
+              onClick={() => setStep(Step.Summary)}
+            />
+          </ul>
+        </nav>
+      </section>
     </main>
   );
 }
