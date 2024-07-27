979cd66f23e47551fa8bd828f9a6abbf1ae15163
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-27 18:46:05 +0200

    test: added pro tests

--- src/convex/__tests__/emails.test.ts
+++ src/convex/__tests__/emails.test.ts
@@ -129,4 +129,76 @@ describe("Emails", () => {
     });
     expect(inactive_workflows.status).toBe("inactive");
   });
+
+  test("Only pro users can access emails", async () => {
+    await t.mutation(internal.users.upsert, {
+      clerk_id: "userid1",
+      workspace: "workspace1",
+      roles: ["org:admin"],
+      plan: "pro",
+    });
+
+    const tAuth1 = t.withIdentity({
+      tokenIdentifier: "https://testingasdf|userid1",
+      // @ts-ignore
+      websiteUrl: "workspace1",
+      gender: "org:admin",
+    });
+
+    try {
+      await tAuth1.mutation(api.emails.create, {
+        title: "Template 1",
+      });
+      expect(true).toBe(true);
+    } catch (error) {
+      expect(true).toBe(false);
+    }
+
+    try {
+      await tAuth1.query(api.emails.search, {
+        paginationOpts: {
+          numItems: 10,
+          cursor: null,
+        },
+      });
+      expect(true).toBe(true);
+    } catch (error) {
+      expect(true).toBe(false);
+    }
+
+    await t.mutation(internal.users.upsert, {
+      clerk_id: "userid2",
+      workspace: "workspace2",
+      roles: ["org:admin"],
+      plan: "free",
+    });
+
+    const tAuth2 = t.withIdentity({
+      tokenIdentifier: "https://testingasdf|userid2",
+      // @ts-ignore
+      websiteUrl: "workspace2",
+      gender: "org:admin",
+    });
+
+    try {
+      await tAuth2.mutation(api.emails.create, {
+        title: "Template 1",
+      });
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(true).toBe(true);
+    }
+
+    try {
+      await tAuth2.query(api.emails.search, {
+        paginationOpts: {
+          numItems: 10,
+          cursor: null,
+        },
+      });
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(true).toBe(true);
+    }
+  });
 });
