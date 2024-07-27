61b1a7d568f7a266c2d25e3fee2a48a3486e50af
AlbaOngaro <alba_ongaro@hotmail.com>
2024-07-27 19:09:28 +0200

    fix: use env for price ids

--- src/convex/stripe/constants.ts
+++ src/convex/stripe/constants.ts
@@ -3,6 +3,6 @@ import Stripe from "stripe";
 export const client = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export const PRICES = {
-  free: "price_1PgZ6JFCTTTwOs5UzuyteYDt",
-  pro: "price_1Pg3fnFCTTTwOs5UnjddiMkg",
+  free: process.env.FREE_PLAN_PRICE_ID,
+  pro: process.env.PRO_PLAN_PRICE_ID,
 };
