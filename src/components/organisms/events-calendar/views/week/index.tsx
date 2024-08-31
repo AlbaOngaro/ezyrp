352404f7fcfe502793dca4de9f0f2b68b6d153ff
AlbaOngaro <alba_ongaro@hotmail.com>
2024-08-31 21:03:05 +0200

    feat: added new overlay component

--- src/components/organisms/events-calendar/views/week/index.tsx
+++ src/components/organisms/events-calendar/views/week/index.tsx
@@ -1,23 +1,17 @@
 import { format } from "date-fns";
 import { Fragment } from "react";
 
-import { get, has } from "lodash";
-import { EventItem } from "../components/event-item";
-import { Indicator } from "../components/indicator";
-import { useCalendarContext } from "../hooks/useCalendarContext";
+import { EventItem } from "../../components/event-item";
+import { Indicator } from "../../components/indicator";
+import { useCalendarContext } from "../../hooks/useCalendarContext";
 
-import { WEEKDAYS } from "../constants";
-import { getGridPosFromTime } from "../helpers";
+import { Overlay } from "./overlay";
 import { cn } from "lib/utils/cn";
-
-import { useSettings } from "hooks/useSettings";
-import { Intervals } from "convex/settings";
 
 export function Body() {
   const {
     state: { days },
   } = useCalendarContext();
-  const { data: settings } = useSettings();
 
   return (
     <div className="isolate flex flex-auto flex-col overflow-auto bg-white">
@@ -253,117 +247,7 @@ export function Body() {
               {days.map((day, i) => {
                 return (
                   <Fragment key={day.date.toISOString()}>
-                    {has(settings, `days.${WEEKDAYS[i]}`) ? (
-                      <>
-                        {(
-                          get(settings, `days.${WEEKDAYS[i]}`, []) as Intervals
-                        ).map(({ start, end }, index, array) => {
-                          const hasMultipleIntervals = array.length > 1;
-                          if (hasMultipleIntervals) {
-                            if (index === 0) {
-                              return (
-                                <Fragment key={index}>
-                                  <div
-                                    style={{
-                                      gridColumnStart: i + 1,
-                                      gridRow: `2 / ${
-                                        getGridPosFromTime(start) * 12 + 2
-                                      }`,
-                                    }}
-                                    className="bg-gray-100/30 pointer-events-none"
-                                  />
-
-                                  <div
-                                    style={{
-                                      gridColumnStart: i + 1,
-                                      gridRow: `${
-                                        getGridPosFromTime(end) * 12 + 2
-                                      } / ${
-                                        getGridPosFromTime(
-                                          array[index + 1].start,
-                                        ) *
-                                          12 +
-                                        2
-                                      }`,
-                                    }}
-                                    className="bg-gray-100/30 pointer-events-none"
-                                  />
-                                </Fragment>
-                              );
-                            }
-
-                            if (index === array.length - 1) {
-                              return (
-                                <div
-                                  key={index}
-                                  style={{
-                                    gridColumnStart: i + 1,
-                                    gridRow: `${
-                                      getGridPosFromTime(end) * 12 + 2
-                                    } / 288`,
-                                  }}
-                                  className="bg-gray-100/30 pointer-events-none"
-                                />
-                              );
-                            }
-
-                            return (
-                              <Fragment key={index}>
-                                <div
-                                  style={{
-                                    gridColumnStart: i + 1,
-                                    gridRow: `${
-                                      getGridPosFromTime(end) * 12 + 2
-                                    } / ${
-                                      getGridPosFromTime(
-                                        array[index + 1].start,
-                                      ) *
-                                        12 +
-                                      2
-                                    }`,
-                                  }}
-                                  className="bg-gray-100/30 pointer-events-none"
-                                />
-                              </Fragment>
-                            );
-                          }
-
-                          return (
-                            <Fragment key={index}>
-                              <div
-                                key={index}
-                                style={{
-                                  gridColumnStart: i + 1,
-                                  gridRow: `2 / ${
-                                    getGridPosFromTime(start) * 12 + 2
-                                  }`,
-                                }}
-                                className="bg-gray-100/30 pointer-events-none"
-                              />
-
-                              <div
-                                key={index}
-                                style={{
-                                  gridColumnStart: i + 1,
-                                  gridRow: `${
-                                    getGridPosFromTime(end) * 12 + 2
-                                  } / 288`,
-                                }}
-                                className="bg-gray-100/30 pointer-events-none"
-                              />
-                            </Fragment>
-                          );
-                        })}
-                      </>
-                    ) : (
-                      <div
-                        className="bg-gray-100/30 pointer-events-none"
-                        style={{
-                          gridColumnStart: i + 1,
-                          gridRow: "2 / -1",
-                        }}
-                      />
-                    )}
+                    <Overlay i={i} day={day} />
 
                     {day.events.map((event) => (
                       <EventItem
